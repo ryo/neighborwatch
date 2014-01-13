9c742eab7726ce7173b9c1c4203d00e2d0f0bfce
Ryo SHIMIZU <ryo@nerv.org>
2014-01-13 16:42:46 +0900

    use "(base 16)" line instead of "(hex)" line

--- ouitxt_convert.pl
+++ ouitxt_convert.pl
@@ -4,16 +4,7 @@
 #   http://www.iana.org/assignments/ethernet-numbers     - Ethernet Numbers
 #   http://standards.ieee.org/regauth/oui/oui.txt        - Vendor codes
 
-
+$H = qr/([\dA-F]{2})/;
 while (<>) {
-	push(@oui, $_);
-}
-
-$HEX = '[\da-fA-F]';
-
-for (@oui) {
-	chop;
-	if (m/^\s+(${HEX}${HEX})-(${HEX}${HEX})-(${HEX}${HEX})\s+\(hex\)\s+(.*)/) {
-		print "$1:$2:$3\t$4\n";
-	}
+	print "$1:$2:$3\t$4\n" if (m/^\s+$H$H$H\s+\(base 16\)\s+(.*)/i);
 }
